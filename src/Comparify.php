b6803258d9b8c64dc7f422fae867d620931f750c
Koen Hoeymans <koenhoeymans@gmail.com>
2014-12-22 17:56:03 +0100

    fix PSR2 coding violations

--- src/Comparify.php
+++ src/Comparify.php
@@ -10,51 +10,45 @@ namespace Comparify;
  */
 class Comparify
 {
-	private $selfClosingTags = 'hr|br|img';
+    private $selfClosingTags = 'hr|br|img';
 
-	private $attributes =
-		"(?J)(?<attributes>
+    private $attributes =
+        "(?J)(?<attributes>
 			(
 			\s+
 			\w+(=(?:\"[^\"]*?\"|\'[^\']*?\'|[^\'\">\s]+))?
 			)*
 		)";
 
-	private function selfClosingElement()
-	{
-		return "
+    private function selfClosingElement()
+    {
+        return "
 			<(?<self_closing_tag>("
-			. $this->selfClosingTags . ")" . $this->attributes . ")[ ]?/?>";
-	}
+            .$this->selfClosingTags.")".$this->attributes.")[ ]?/?>";
+    }
 
-	private function element(array $allowedTags = null, array $deniedTags = null)
-	{
-		if ($allowedTags === null)
-		{
-			$allowedTags = '\w+';
-		}
-		else
-		{
-			$allowedTags = implode('|', $allowedTags); 
-		}
+    private function element(array $allowedTags = null, array $deniedTags = null)
+    {
+        if ($allowedTags === null) {
+            $allowedTags = '\w+';
+        } else {
+            $allowedTags = implode('|', $allowedTags);
+        }
 
-		if ($deniedTags === null)
-		{
-			$deniedTags = '';
-		}
-		else
-		{
-			$deniedTags = '(?!' . implode('|', $deniedTags) . ')';
-		}
+        if ($deniedTags === null) {
+            $deniedTags = '';
+        } else {
+            $deniedTags = '(?!'.implode('|', $deniedTags).')';
+        }
 
-		return
-			"
+        return
+            "
 			(?<element>
 			(?<full_tag>
 				<"
-					. $deniedTags . "(?<tag>" . $allowedTags . ")"
-					. $this->attributes
-				. ">
+                    .$deniedTags."(?<tag>".$allowedTags.")"
+                    .$this->attributes
+                .">
 			)
 				(?<content>
 					(
@@ -62,180 +56,174 @@ class Comparify
 						|
 						<code>[^<]+?</code>
 						|
-						<(?<subtag>\w+)" . $this->attributes . ">
+						<(?<subtag>\w+)".$this->attributes.">
 						(?&content)
 						</\g{subtag}>
 						|
-						" . $this->selfClosingElement() . "
+						".$this->selfClosingElement()."
 					)*
 				)
 			</\g{tag}>
 			)";
-	}
+    }
 
-	/**
-	 * @param string $input
-	 * @return string
-	 */
-	public function transform($text)
-	{
-		$text = $this->handleSelfClosingTags($text);
-		$text = $this->openClosedElements($text);
-		$text = $this->removeSpacingOnBlankLines($text);
-		$text = $this->setOpeningTagsOnOneLine($text);
-		$text = $this->setEmptyTagsOnOneLine($text);
-		$text = $this->removeWhitespaceBeforeTagsOnOwnLine($text);
-		$text = $this->setSelectedElementsOnOwnLine($text);
-		$text = $this->removeBlankLineInsideElement($text);
-		$text = $this->removeBlankLineBetweenElements($text);
-		$text = $this->trimeWhitespaceAtStartAndEndInsideElements($text);
-		$text = trim($text);
+    /**
+     * @param  string $input
+     * @return string
+     */
+    public function transform($text)
+    {
+        $text = $this->handleSelfClosingTags($text);
+        $text = $this->openClosedElements($text);
+        $text = $this->removeSpacingOnBlankLines($text);
+        $text = $this->setOpeningTagsOnOneLine($text);
+        $text = $this->setEmptyTagsOnOneLine($text);
+        $text = $this->removeWhitespaceBeforeTagsOnOwnLine($text);
+        $text = $this->setSelectedElementsOnOwnLine($text);
+        $text = $this->removeBlankLineInsideElement($text);
+        $text = $this->removeBlankLineBetweenElements($text);
+        $text = $this->trimeWhitespaceAtStartAndEndInsideElements($text);
+        $text = trim($text);
 
-		return $text;
-	}
+        return $text;
+    }
 
-	private function handleSelfClosingTags($text)
-	{
-		return preg_replace('@' . $this->selfClosingElement() . '@', '<\1 \2/>', $text);
-	}
+    private function handleSelfClosingTags($text)
+    {
+        return preg_replace('@'.$this->selfClosingElement().'@', '<\1 \2/>', $text);
+    }
 
-	private function openClosedElements($text)
-	{
-		return preg_replace(
-			'@(?!' . $this->selfClosingElement() . ')<(?<tag>\w+)(' . $this->attributes . ')[ ]/>@x',
-			'<\6\7></\6>',
-			$text
-		);
-	}
+    private function openClosedElements($text)
+    {
+        return preg_replace(
+            '@(?!'.$this->selfClosingElement().')<(?<tag>\w+)('.$this->attributes.')[ ]/>@x',
+            '<\6\7></\6>',
+            $text
+        );
+    }
 
-	private  function removeSpacingOnBlankLines($text)
-	{
-		return preg_replace("@(?<=^|\n)([ ]|\t)+(?=\n|$)@", "", $text);
-	}
+    private function removeSpacingOnBlankLines($text)
+    {
+        return preg_replace("@(?<=^|\n)([ ]|\t)+(?=\n|$)@", "", $text);
+    }
 
-	private function setOpeningTagsOnOneLine($text)
-	{
-		$pattern = '@' . $this->element() . '@x';
+    private function setOpeningTagsOnOneLine($text)
+    {
+        $pattern = '@'.$this->element().'@x';
 
-		return preg_replace_callback(
-			$pattern,
-			function($match)
-			{
-				$openingTag = preg_replace("@\n([ ]+|\t+)@", ' ', $match['full_tag']);
-				return $openingTag . $match['content'] . '</' . $match['tag'] . '>';
-			},
-			$text
-		);
-	}
+        return preg_replace_callback(
+            $pattern,
+            function ($match) {
+                $openingTag = preg_replace("@\n([ ]+|\t+)@", ' ', $match['full_tag']);
 
-	private function setEmptyTagsOnOneLine($text)
-	{
-		$pattern = '@' . $this->selfClosingElement() . '@x';
+                return $openingTag.$match['content'].'</'.$match['tag'].'>';
+            },
+            $text
+        );
+    }
 
-		return preg_replace_callback(
-			$pattern,
-			function($match)
-			{
-				return
-					'<'
-					. preg_replace("@\n([ ]+|\t+)@", ' ', $match['self_closing_tag'])
-					. ' />';
-			},
-			$text
-		);
-	}
+    private function setEmptyTagsOnOneLine($text)
+    {
+        $pattern = '@'.$this->selfClosingElement().'@x';
 
-	private function removeBlankLineBetweenElements($text)
-	{
-		$pattern = "@" . $this->element() . "[\n][\n]+	@x";
+        return preg_replace_callback(
+            $pattern,
+            function ($match) {
+                return
+                    '<'
+                    .preg_replace("@\n([ ]+|\t+)@", ' ', $match['self_closing_tag'])
+                    .' />';
+            },
+            $text
+        );
+    }
 
-		return preg_replace_callback(
-			$pattern,
-			function($match)
-			{
-				return $match['element'] . "\n";
-			},
-			$text
-		);
-	}
+    private function removeBlankLineBetweenElements($text)
+    {
+        $pattern = "@".$this->element()."[\n][\n]+	@x";
 
-	private function removeBlankLineInsideElement($text)
-	{
-		$pattern = "@(?<=^|[\n])" . $this->element(null, array('code')) . "(?=[\n]|$)@x";
+        return preg_replace_callback(
+            $pattern,
+            function ($match) {
+                return $match['element']."\n";
+            },
+            $text
+        );
+    }
 
-		return preg_replace_callback(
-			$pattern,
-			function($match)
-			{
-				$content = $this->removeBlankLineInsideElement($match['content']);
-				$content = preg_replace("@\n\n@", "\n", $content);
-				return $match['full_tag'] . $content . '</' . $match['tag'] . ">";
-			},
-			$text
-		);		
-	}
+    private function removeBlankLineInsideElement($text)
+    {
+        $pattern = "@(?<=^|[\n])".$this->element(null, array('code'))."(?=[\n]|$)@x";
 
-	private function setSelectedElementsOnOwnLine($text)
-	{
-		$tags = array(
-			'blockquote', 'dd', 'dl', 'dt', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
-			'li', 'ol', 'p', 'pre', 'table', 'tbody', 'td', 'tfoot',
-			'th', 'thead', 'tr', 'ul'
-		);
-		$pattern = "@[\n]*((?J)"
-			. $this->element($tags) . "|" . $this->selfClosingElement()
-			. ")[\n]*@x";
+        return preg_replace_callback(
+            $pattern,
+            function ($match) {
+                $content = $this->removeBlankLineInsideElement($match['content']);
+                $content = preg_replace("@\n\n@", "\n", $content);
 
-		return preg_replace_callback(
-			$pattern,
-			function($match)
-			{
-				$content = $this->setSelectedElementsOnOwnLine($match['content']);
-				if (!empty($match['self_closing_tag']))
-				{
-					return "\n<" . $match['self_closing_tag'] . " />\n";
-				}
-				else
-				{
-				return "\n"
-					. $match['full_tag'] . $content
-					. '</' . $match['tag'] . ">\n";
-				}
-			},
-			$text
-		);
-	}
+                return $match['full_tag'].$content.'</'.$match['tag'].">";
+            },
+            $text
+        );
+    }
 
-	private function removeWhitespaceBeforeTagsOnOwnLine($text)
-	{
-		$pattern =
-			"@
+    private function setSelectedElementsOnOwnLine($text)
+    {
+        $tags = array(
+            'blockquote', 'dd', 'dl', 'dt', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+            'li', 'ol', 'p', 'pre', 'table', 'tbody', 'td', 'tfoot',
+            'th', 'thead', 'tr', 'ul',
+        );
+        $pattern = "@[\n]*((?J)"
+            .$this->element($tags)."|".$this->selfClosingElement()
+            .")[\n]*@x";
+
+        return preg_replace_callback(
+            $pattern,
+            function ($match) {
+                $content = $this->setSelectedElementsOnOwnLine($match['content']);
+                if (!empty($match['self_closing_tag'])) {
+                    return "\n<".$match['self_closing_tag']." />\n";
+                } else {
+                    return "\n"
+                    .$match['full_tag'].$content
+                    .'</'.$match['tag'].">\n";
+                }
+            },
+            $text
+        );
+    }
+
+    private function removeWhitespaceBeforeTagsOnOwnLine($text)
+    {
+        $pattern =
+            "@
 			(?<=[\n]|^)
 				([ \t]+)
 				(
-					<(\w+)" . $this->attributes . ">
+					<(\w+)".$this->attributes.">
 					|
 					</(\w+)>
 				)
 			@x";
 
-		return preg_replace($pattern, '\2', $text);
-	}
+        return preg_replace($pattern, '\2', $text);
+    }
 
-	private function trimeWhitespaceAtStartAndEndInsideElements($text)
-	{
-		$tags = array('p');
-		$pattern = "@" . $this->element($tags) . "@x";
-		
-		return preg_replace_callback(
-				$pattern,
-				function($match)
-				{
-					$content = $this->trimeWhitespaceAtStartAndEndInsideElements($match['content']);
-					return $match['full_tag'] . trim($content)
-							. '</' . $match['tag'] . ">";
-				},
-				$text
-		);
-	}
+    private function trimeWhitespaceAtStartAndEndInsideElements($text)
+    {
+        $tags = array('p');
+        $pattern = "@".$this->element($tags)."@x";
+
+        return preg_replace_callback(
+                $pattern,
+                function ($match) {
+                    $content = $this->trimeWhitespaceAtStartAndEndInsideElements($match['content']);
+
+                    return $match['full_tag'].trim($content)
+                            .'</'.$match['tag'].">";
+                },
+                $text
+        );
+    }
 }
--- tests/unit/ComparifyTest.php
+++ tests/unit/ComparifyTest.php
@@ -4,97 +4,97 @@ namespace Comparify;
 
 class ComparifyTest extends \PHPUnit_Framework_TestCase
 {
-	public function setup()
-	{
-		$this->comparify = new \Comparify\Comparify();
-	}
+    public function setup()
+    {
+        $this->comparify = new \Comparify\Comparify();
+    }
 
-	/**
-	 * @test
-	 */
-	public function returnsString()
-	{
-		$this->assertTrue(is_string($this->comparify->transform(('foo'))));
-	}
+    /**
+     * @test
+     */
+    public function returnsString()
+    {
+        $this->assertTrue(is_string($this->comparify->transform(('foo'))));
+    }
 
-	/**
-	 * @test
-	 */
-	public function stripsWhitespaceOfStart()
-	{
-		$this->assertEquals('foo', $this->comparify->transform(' foo'));
-	}
+    /**
+     * @test
+     */
+    public function stripsWhitespaceOfStart()
+    {
+        $this->assertEquals('foo', $this->comparify->transform(' foo'));
+    }
 
-	/**
-	 * @test
-	 */
-	public function stripsWhitespaceOfEnd()
-	{
-		$this->assertEquals('foo', $this->comparify->transform('foo '));
-	}
+    /**
+     * @test
+     */
+    public function stripsWhitespaceOfEnd()
+    {
+        $this->assertEquals('foo', $this->comparify->transform('foo '));
+    }
 
-	/**
-	 * @test
-	 */
-	public function addsWhitespaceBeforeClosingOfSelfClosingTag()
-	{
-		$this->assertEquals('<br />', $this->comparify->transform('<br/>'));
-	}
+    /**
+     * @test
+     */
+    public function addsWhitespaceBeforeClosingOfSelfClosingTag()
+    {
+        $this->assertEquals('<br />', $this->comparify->transform('<br/>'));
+    }
 
-	/**
-	 * @test
-	 */
-	public function removesBlankLineAfterTag()
-	{
-		$text = "<p>paragraph</p>
-
-<p>other paragraph</p>";
-
-		$result = "<p>paragraph</p>
-<p>other paragraph</p>";
-
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
-
-	/**
-	 * @test
-	 */
-	public function removesMultipleBlankLinesAfterTag()
-	{
-		$text = "<p>paragraph</p>
-
-
+    /**
+     * @test
+     */
+    public function removesBlankLineAfterTag()
+    {
+        $text = "<p>paragraph</p>
 
 <p>other paragraph</p>";
 
-		$result = "<p>paragraph</p>
+        $result = "<p>paragraph</p>
 <p>other paragraph</p>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function removesMultipleBlankLines_2()
-	{
-		$text = "<p>This is the <b>simple case</b></p>
+    /**
+     * @test
+     */
+    public function removesMultipleBlankLinesAfterTag()
+    {
+        $text = "<p>paragraph</p>
+
+
+
+<p>other paragraph</p>";
+
+        $result = "<p>paragraph</p>
+<p>other paragraph</p>";
+
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
+
+    /**
+     * @test
+     */
+    public function removesMultipleBlankLines_2()
+    {
+        $text = "<p>This is the <b>simple case</b></p>
 
 
 <p>This one has a <b>line break</b></p>";
 
-		$result = "<p>This is the <b>simple case</b></p>
+        $result = "<p>This is the <b>simple case</b></p>
 <p>This one has a <b>line break</b></p>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function removesBlankLinesAfterCodeElementInsideOtherElement()
-	{
-		$text = "<div>
+    /**
+     * @test
+     */
+    public function removesBlankLinesAfterCodeElementInsideOtherElement()
+    {
+        $text = "<div>
 
 <code>
 foo
@@ -104,41 +104,41 @@ foo
 
 ";
 
-		$result = "<div>
+        $result = "<div>
 <code>
 foo
 </code>
 </div>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function removesBlankLinesAfterDoubleCodeElementInsideOtherElement()
-	{
-		$text = "<p><code>foo</code></p>
+    /**
+     * @test
+     */
+    public function removesBlankLinesAfterDoubleCodeElementInsideOtherElement()
+    {
+        $text = "<p><code>foo</code></p>
 <pre><code>foo</code></pre>
 
 
 <p><code>foo</code></p>
 <p>paragraph</p>";
 
-		$result = "<p><code>foo</code></p>
+        $result = "<p><code>foo</code></p>
 <pre><code>foo</code></pre>
 <p><code>foo</code></p>
 <p>paragraph</p>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function elementDoesNotAcceptLesserThanSignInsideCode()
-	{
-		$text = "<pre><code>foo</code></pre>
+    /**
+     * @test
+     */
+    public function elementDoesNotAcceptLesserThanSignInsideCode()
+    {
+        $text = "<pre><code>foo</code></pre>
 <p>a</p>
 
 <p>Markdown:</p>
@@ -147,277 +147,277 @@ foo
 
 ";
 
-		$result = "<pre><code>foo</code></pre>
+        $result = "<pre><code>foo</code></pre>
 <p>a</p>
 <p>Markdown:</p>
 <pre><code>foo</code></pre>
 <pre><code>foo</code></pre>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function removesBlankLineAfterElementContainingSelfClosingElement()
-	{
-		$text = "<div>
+    /**
+     * @test
+     */
+    public function removesBlankLineAfterElementContainingSelfClosingElement()
+    {
+        $text = "<div>
 <img />
 
 </div>";
 
-		$result = "<div>
+        $result = "<div>
 <img />
 </div>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function removesBlankLinesAfterTagRecursive()
-	{
-		$text = "<div>
+    /**
+     * @test
+     */
+    public function removesBlankLinesAfterTagRecursive()
+    {
+        $text = "<div>
 <p>paragraph</p>
 
 <p>other paragraph</p>
 </div>";
 
-		$result = "<div>
+        $result = "<div>
 <p>paragraph</p>
 <p>other paragraph</p>
 </div>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));		
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function removesSpacesOnBlankLines()
-	{
-		$text = "<p>foo</p>
- 
+    /**
+     * @test
+     */
+    public function removesSpacesOnBlankLines()
+    {
+        $text = "<p>foo</p>
+
 <p>bar</p>";
 
-		$result = "<p>foo</p>
+        $result = "<p>foo</p>
 <p>bar</p>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function removesTabsOnBlankLines()
-	{
-		$text = "<p>foo</p>
-	
+    /**
+     * @test
+     */
+    public function removesTabsOnBlankLines()
+    {
+        $text = "<p>foo</p>
+
 <p>bar</p>";
 
-		$result = "<p>foo</p>
+        $result = "<p>foo</p>
 <p>bar</p>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function removesBlankLineInsideElement()
-	{
-		$text = "<a>
+    /**
+     * @test
+     */
+    public function removesBlankLineInsideElement()
+    {
+        $text = "<a>
 
 foo
 </a>";
 
-		$result = "<a>
+        $result = "<a>
 foo
 </a>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function doesntRemoveBlankLineInsideCodeElements()
-	{
-		$text = "<code>
-
-bar
-</code>";
-
-		$result = "<code>
+    /**
+     * @test
+     */
+    public function doesntRemoveBlankLineInsideCodeElements()
+    {
+        $text = "<code>
 
 bar
 </code>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));		
-	}
+        $result = "<code>
 
-	/**
-	 * @test
-	 */
-	public function setsHeaderElementsOnOwnLine()
-	{
-		$text = "<p>paragraph</p><h1>header</h1>";
+bar
+</code>";
 
-		$result = "<p>paragraph</p>
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
+
+    /**
+     * @test
+     */
+    public function setsHeaderElementsOnOwnLine()
+    {
+        $text = "<p>paragraph</p><h1>header</h1>";
+
+        $result = "<p>paragraph</p>
 <h1>header</h1>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));		
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function setsBlockquotesOnOwnLine()
-	{
-		$text = "<p>paragraph</p><blockquote>foo</blockquote>";
+    /**
+     * @test
+     */
+    public function setsBlockquotesOnOwnLine()
+    {
+        $text = "<p>paragraph</p><blockquote>foo</blockquote>";
 
-		$result = "<p>paragraph</p>
+        $result = "<p>paragraph</p>
 <blockquote>foo</blockquote>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function setsParagraphsOnOwnLine()
-	{
-		$text = "<a>b</a><p>paragraph</p>";
+    /**
+     * @test
+     */
+    public function setsParagraphsOnOwnLine()
+    {
+        $text = "<a>b</a><p>paragraph</p>";
 
-		$result = "<a>b</a>
+        $result = "<a>b</a>
 <p>paragraph</p>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function setsOrderedListItemsOnOwnLine()
-	{
-		$text = "<ul><li>item</li><li>item</li></ul>";
+    /**
+     * @test
+     */
+    public function setsOrderedListItemsOnOwnLine()
+    {
+        $text = "<ul><li>item</li><li>item</li></ul>";
 
-		$result = "<ul>
+        $result = "<ul>
 <li>item</li>
 <li>item</li>
 </ul>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function setsHorizontalRuleOnOwnLine()
-	{
-		$text = "<div>foo</div><hr />";
+    /**
+     * @test
+     */
+    public function setsHorizontalRuleOnOwnLine()
+    {
+        $text = "<div>foo</div><hr />";
 
-		$result = "<div>foo</div>
+        $result = "<div>foo</div>
 <hr />";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function setsListItemsContainingElementOnOwnLine()
-	{
-		$text = "<ol><li><em>a</em></li><li><em>b</em></li></ol>";
+    /**
+     * @test
+     */
+    public function setsListItemsContainingElementOnOwnLine()
+    {
+        $text = "<ol><li><em>a</em></li><li><em>b</em></li></ol>";
 
-		$result = "<ol>
+        $result = "<ol>
 <li><em>a</em></li>
 <li><em>b</em></li>
 </ol>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function removesWhiteSpaceBeforeTagsOnOwnLine()
-	{
-		$text = "<foo>bar</foo>
+    /**
+     * @test
+     */
+    public function removesWhiteSpaceBeforeTagsOnOwnLine()
+    {
+        $text = "<foo>bar</foo>
  <a>b</a>";
 
-		$result = "<foo>bar</foo>
+        $result = "<foo>bar</foo>
 <a>b</a>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));		
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function tagsOnMultipleLinesArePutOnOneLine()
-	{
-		$text = "<foo
+    /**
+     * @test
+     */
+    public function tagsOnMultipleLinesArePutOnOneLine()
+    {
+        $text = "<foo
 	id=\"bar\">bar</foo>";
 
-		$result = "<foo id=\"bar\">bar</foo>";
+        $result = "<foo id=\"bar\">bar</foo>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));		
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function setsHorizontalRuleOnOneLine()
-	{
-		$text = "<hr
+    /**
+     * @test
+     */
+    public function setsHorizontalRuleOnOneLine()
+    {
+        $text = "<hr
 	id=\"bar\" >";
 
-		$result = "<hr id=\"bar\" />";
+        $result = "<hr id=\"bar\" />";
 
-		$this->assertEquals($result, $this->comparify->transform($text));		
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function setsPreOnOwnLine()
-	{
-		$text = "<li><pre><code>code block
+    /**
+     * @test
+     */
+    public function setsPreOnOwnLine()
+    {
+        $text = "<li><pre><code>code block
 as first element of a list item
 </code></pre></li>";
 
-		$result = "<li>
+        $result = "<li>
 <pre><code>code block
 as first element of a list item
 </code></pre>
 </li>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function opensClosedElementsOtherThanSelfClosing()
-	{
-		$text = "<div />";
+    /**
+     * @test
+     */
+    public function opensClosedElementsOtherThanSelfClosing()
+    {
+        $text = "<div />";
 
-		$result = "<div></div>";
+        $result = "<div></div>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 
-	/**
-	 * @test
-	 */
-	public function removesWhiteSpaceAtStartOfParagraph()
-	{
-		$text = "<p> A paragraph</p>";
+    /**
+     * @test
+     */
+    public function removesWhiteSpaceAtStartOfParagraph()
+    {
+        $text = "<p> A paragraph</p>";
 
-		$result = "<p>A paragraph</p>";
+        $result = "<p>A paragraph</p>";
 
-		$this->assertEquals($result, $this->comparify->transform($text));
-	}
+        $this->assertEquals($result, $this->comparify->transform($text));
+    }
 }
